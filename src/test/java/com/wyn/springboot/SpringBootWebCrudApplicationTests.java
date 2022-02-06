42e00a2d78f2a18c0f74ee3bb582a7ae937fd13f
wei-ya-nan <soyongman0@gmail.com>
2022-02-06 15:26:31 +0800

    update commit

---
+++ src/test/java/com/wyn/springboot/SpringBootWebCrudApplicationTests.java
@@ -0,0 +1,22 @@
+package com.wyn.springboot;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class SpringBootWebCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+        System.out.println("second");
+        System.out.println("thrid");
+        System.out.println("hot-fix");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("push test");
+    }
+
+}
