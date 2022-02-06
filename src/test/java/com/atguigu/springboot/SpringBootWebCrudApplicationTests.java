9695115ffda790356c411134cbebb25d64f31c75
wei-ya-nan <soyongman0@gmail.com>
2022-02-06 14:52:16 +0800

    update one

---
+++ src/test/java/com/atguigu/springboot/SpringBootWebCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.springboot;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class SpringBootWebCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+}
